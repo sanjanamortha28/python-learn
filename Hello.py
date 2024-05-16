9f7d47c1e10a9a329203ebc659ac80d5d0b7f5ab
Sanjana Mortha <70304377+sanjanamortha28@users.noreply.github.com>
2024-05-16 13:33:30 -0500

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,15 @@
+print('Hello python')
+
+h = ['sanju','mahi',1,2,2,2]
+final=h.append("jay")
+print(h)
+print(h.count('m'))
+
+HeyTuple = ("sanjanamortha")
+s = tuple(HeyTuple)
+print(s)
+
+Dict = {}
+Dict[3] = {"sanju": {"Hey",3}}
+print(Dict)
+
